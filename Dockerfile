ef716344bcb67a43225e9649e196606e174286c8
Joelle Gilley <gilley.joelle@gmail.com>
2018-06-27 10:40:12 -0400

    Initial checkin

---
+++ Dockerfile
@@ -0,0 +1,66 @@
+FROM php:7.2-fpm-alpine3.7
+
+ENV TIMEZONE=UTC \
+	ENV=/etc/profile \
+	APP_ENV=development
+
+RUN apk --update add dumb-init ca-certificates nginx supervisor bash \
+		tzdata unzip zip && \
+	apk add --virtual .build_package git curl build-base autoconf dpkg-dev \
+		file libmagic re2c && \
+	apk add --virtual .deps_run bzip2 libjpeg-turbo libpng libmcrypt freetype \
+		icu libcurl && \
+	apk add --virtual .build_deps bzip2-dev libjpeg-turbo-dev libpng-dev \
+		libmcrypt-dev freetype-dev icu-dev curl-dev
+
+# setup timezone
+# update ca certs
+# prep bash environmnet
+# pepare nginx/php run and app directories
+RUN cp /usr/share/zoneinfo/${TIMEZONE} /etc/localtime && \
+	echo "${TIMEZONE}" > /etc/timezone && \
+	update-ca-certificates && \
+	mv /etc/profile.d/color_prompt /etc/profile.d/color_prompt.sh && \
+	echo alias dir=\'ls -alh --color\' >> /etc/profile && \
+	echo 'source ~/.profile' >> /etc/profile && \
+	echo 'cat /etc/os-release' >> ~/.profile && \
+	rm -rf /etc/nginx/conf.d/default.conf && \
+	mkdir -p /app /run/nginx /run/php /var/lib/nginx/logs && \
+	chown -R nginx:www-data /run/nginx /var/lib/nginx/logs && \
+	chown -R www-data:www-data /run/php /app && \
+	chmod -R g+rws /app
+
+# install php modules
+RUN docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ && \
+	docker-php-ext-install intl pdo pdo_mysql curl gd && \
+	pecl install -o -f xdebug mcrypt && \
+	docker-php-ext-enable xdebug mcrypt
+
+RUN curl --silent --show-error https://getcomposer.org/installer | php && \
+	mv /var/www/html/composer.phar /usr/local/bin/composer
+
+# Clean up
+RUN apk del .build_package .build_deps && \
+	{ find /usr/local/lib -type f -print0 | xargs -0r strip --strip-all -p 2>/dev/null || true; } && \
+	rm -rf /tmp/* /usr/local/lib/php/doc/* /var/cache/apk/*
+
+
+# copy our config files over to the container
+COPY ./container_configs/php-fpm.conf /usr/local/etc/php-fpm.conf
+COPY ./container_configs/nginx.conf /etc/nginx/nginx.conf
+COPY ./container_configs/default.conf /etc/nginx/conf.d/default.conf
+
+# Report on PHP build
+RUN php -v && \
+	php -m
+
+WORKDIR /app
+
+# expose our service port
+EXPOSE 80
+
+# start with our PID 1 controller
+ENTRYPOINT ["/usr/bin/dumb-init", "--"]
+
+# what we use to start the container
+CMD ["/bin/sh", "-c", "php-fpm --daemonize && nginx -g \"daemon off;\""]
---
+++ code/index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>404 - Please stand by</title>
+	<meta name="author" content="Ryan McGuire">
+
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<style>
+	.author a,.color,.color2,.color3,.warning{position:absolute;text-align:center}a,abbr,acronym,address,article,aside,audio,b,big,blockquote,canvas,caption,center,cite,code,dd,del,details,dfn,dl,dt,em,embed,fieldset,figcaption,figure,footer,form,h1,h2,h3,h4,h5,h6,header,hgroup,i,img,ins,kbd,label,legend,li,mark,menu,nav,ol,output,p,pre,q,ruby,s,samp,section,small,strike,strong,sub,summary,sup,table,tbody,td,tfoot,th,thead,time,tr,tt,u,ul,var,video{margin:0;padding:0;border:0;font:inherit;vertical-align:baseline;font-family:Helvetica Neue,Helvetica,Arial,sans-serif}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}body{padding:0; margin:0;line-height:1}ol,ul{list-style:none}blockquote,q{quotes:none}blockquote:after,blockquote:before,q:after,q:before{content:'';content:none}table{border-collapse:collapse;border-spacing:0}.color{float:left;height:67%;width:14.29%}.color:nth-child(1){left:0}.color:nth-child(2){left:14.28%}.color:nth-child(3){left:28.57%}.color:nth-child(4){left:42.85%}.color:nth-child(5){left:57.14%}.color:nth-child(6){left:71.42%}.color:nth-child(7){left:85.71%}.color2{float:left;height:75%;width:14.2857142857%}.color2:nth-child(1){left:0}.color2:nth-child(2){left:14.2857142857%}.color2:nth-child(3){left:28.5714285714%}.color2:nth-child(4){left:42.8571428571%}.color2:nth-child(5){left:57.1428571428%}.color2:nth-child(6){left:71.4285714285%}.color2:nth-child(7){left:85.7142857142%}.color3{float:left;height:100%;width:16.68%}.color3:nth-child(1){left:0}.color3:nth-child(2){left:16.66%}.color3:nth-child(3){left:33.33%}.color3:nth-child(4){left:50%}.color3:nth-child(5){left:66.66%}.color3:nth-child(6){left:83.33%}.white{background-color:#fff}.yellow{background-color:#dcdc09}.teal{background-color:#21cece}.green{background-color:#10d210}.pink{background-color:#cf12cf}.red{background-color:#d31212}.blue{background-color:#2323d5}.black{background-color:#000}.blue2{background-color:#03295b}.purple{background-color:#3e057e}.gray{background-color:#131313}.gray2{background-color:#1d1d1d}.gray3{background-color:#d5d2d2}.warning{z-index:1000;width:100%;top:28%}.warning h1{color:#fff;text-transform:uppercase;letter-spacing:2px;font-size:5em;padding:5px 25px;font-weight:700;text-shadow:0 1px 3px rgba(0,0,0,.12)}.warning h2{color:#fff;letter-spacing:1px;font-size:1.3em;padding:5px 25px 50px;font-weight:400;text-shadow:0 1px 2px rgba(0,0,0,.13)}.author a,.btn{text-decoration:none;text-transform:uppercase;color:#000}.btn{-webkit-border-radius:0;-moz-border-radius:0;border-radius:0;font-size:.9em;letter-spacing:1.5px;background-color:#fff;padding:18px 27px;font-weight:400}.btn:hover{opacity:.9}@media all and (max-width:950px){.warning h1{letter-spacing:2px;font-size:4em}}@media all and (max-width:768px){.warning{top:20%}.warning h1{letter-spacing:2px;font-size:2.5em}.warning h2{font-size:.8em;padding-bottom:40px}.btn{font-size:.7em;color:#000;padding:12px 20px}}.author a{bottom:0;right:0;padding: 0 15px 15px 0;z-index:1001;font-size:.7em;letter-spacing:2px}@media all and (max-width:450px){.warning h1{letter-spacing:2px;font-size:1.5em}.author a{font-size:.5em}}
+</style>
+</head>
+<body>
+	<div class="warning">
+		<h1>Please Stand By</h1>
+		<h2>The page you were looking for is experiencing technical difficulites.</h2>
+		<a href="index.html" class="btn">Change the Channel</a>
+	</div>
+	<div id="color3">
+		<div class="color3 blue2"></div>
+		<div class="color3 white"></div>
+		<div class="color3 purple"></div>
+		<div class="color3 black"></div>
+		<div class="color3 gray"></div>
+		<div class="color3 gray2"></div>
+	</div> 
+	<div id="color2">
+		<div class="color2 blue"></div>
+		<div class="color2 black"></div>
+		<div class="color2 pink"></div>
+		<div class="color2 black"></div>
+		<div class="color2 teal"></div>
+		<div class="color2 black"></div>
+		<div class="color2 gray3"></div>
+	</div> 
+	<div id="color">
+		<div class="color gray3"></div>
+		<div class="color yellow"></div>
+		<div class="color teal"></div>
+		<div class="color green"></div>
+		<div class="color pink"></div>
+		<div class="color red"></div>
+		<div class="color blue"></div>
+	</div>
+	<div class="author"><a href="http://www.bellsdesign.com">McGuire Made</a></div> 
+</body>
+</html>
